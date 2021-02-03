3b6389587a26321aebb83e663afe827b4cb83909
Enoch2k2 <enoch2k2@hotmail.com>
2021-02-03 11:44:44 -0600

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :cartoons
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root to: "static#home"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Cartoon Routes
+  resources :cartoons
+
+
+  # User Routes
+
+
+
+  # Favorite Routes
 end
